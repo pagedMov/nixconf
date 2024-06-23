267b7006a5614bf4cdf7db9c49d326b2ecf11f8a
Kyler Clay <pagedmov@gmail.com>
2024-06-23 04:02:14 -0400

    Update check services working now

--- nixsys/scripts/updatecheck.sh
+++ nixsys/scripts/updatecheck.sh
@@ -1,19 +1,26 @@
 #!/run/current-system/sw/bin/bash
 
+export HOME="/root"
+export PATH=$PATH:/run/current-system/sw/bin
+export NIX_PATH=/home/shade/.nix-defexpr/channels:nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos:nixos-config=/etc/nixos/configuration.nix:/nix/var/nix/profiles/per-user/root/channels
+git config --global --add safe.directory /home/shade/.config/home-manager
 prevdir="$PWD"
 cd /home/shade/.config/home-manager
 
-git fetch origin
+sudo -u shade git fetch origin
 
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse @{u})
+LOCAL=$(sudo -u shade git rev-parse @)
+REMOTE=$(sudo -u shade git rev-parse @{u})
+
 
 if [ "$LOCAL" != "$REMOTE" ]; then
 	echo "Update found"
 	echo "Synchronizing configuration with repository..."
-	git pull
-	rsync "/home/shade/.config/home-manager/nixsys/" "/etc/nixos/"
-	nix-rebuild switch
+	sudo -u shade git pull
+	sudo -u shade rsync "/home/shade/.config/home-manager/nixsys/" "/etc/nixos/"
+	nixos-rebuild switch
 	echo "1" > .update
+else
+	echo "No update found"
 fi
 cd "$prevdir"
