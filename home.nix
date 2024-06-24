e275c2d775e2260fd99eb223311ca0225508e5fc
Kyler Clay <pagedmov@gmail.com>
2024-06-24 16:45:36 -0400

    re-added packages into home.nix after accidental deletion

--- home.nix
+++ home.nix
@@ -31,7 +31,77 @@ home.file = {
 	".config/nvim/init.lua".source =		dotfiles/nvim-init.lua;
 	".config/neovide/init.lua".source =		dotfiles/nvim-init.lua;
 };
-
+home.packages = with pkgs; [
+	firefox
+	jetbrains-mono
+	nerdfonts
+	waybar
+	tree
+	rofi-wayland
+	hello
+	hyprpaper
+	hyprpicker
+	gimp
+	vlc
+	audacity
+	spotify
+	spotifyd
+	nwg-look
+	glib
+	zathura
+	font-awesome
+	nerdfonts
+	chromium
+	p7zip
+	wineWayland
+	grc
+	discord
+	slurp
+	grimblast
+	playerctl
+	waybar-mpris
+	spotify-player
+	vimgolf
+	ncurses
+	google-chrome
+	thunderbird
+	galculator
+	virt-manager
+	godot_4
+	blender
+	jdk
+	ant
+	mono
+	w3m
+	brave
+	wl-clipboard
+	libreoffice
+	gnumake
+	eww
+	vbam
+	gdb
+	qimgv
+	bc
+	socat
+	hyprland-workspaces
+	uhk-udev-rules
+	uhk-agent
+	qbittorrent
+	mcomix
+	libnotify
+	dunst
+	yt-dlp
+	brightnessctl
+	xdotool
+	neovim
+	vimPlugins.lazy-nvim
+	pyright
+	lua-language-server
+	lsof
+	service-wrapper
+	clang-tools
+	neovide
+];
 gtk = {
 	enable = true;
 	theme = {
